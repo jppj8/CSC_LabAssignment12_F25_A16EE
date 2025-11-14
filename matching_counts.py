c7ee6d9f10766074df41c34f90c60c3414c08e5c
jppj8 <jtejada@broncos.uncfsu.edu>
2025-11-13 21:56:51 -0500

    Implement count_matching function logic
    Implemented counting logic for matching elements in a nested list.

--- matching_counts.py
+++ matching_counts.py
@@ -12,14 +12,19 @@ def count_matching(matrix, target):
     @return [int]: the number of matching elements
     """
 
-    # TODO: set initial variable `count`
+    # set initial variable `count`
+    count = 0
 
-    # TODO: use external loop to get all nested list
-    # TODO: use inner loop to get every element of the inner list, if the element is equal to the target argument, increase `count` by 1
+    # external loop to get all nested lists (rows)
+    for row in matrix:
+        # inner loop to get every element in the row
+        for value in row:
+            if value == target:
+                count += 1
 
     return count
 
 
 if __name__ == "__main__":
     m = [[1, 2, 1], [0, 3, 4], [1, 1, 0]]
-    print(count_matching(m, 1))
+    print(count_matching(m, 1))  # should print 4
